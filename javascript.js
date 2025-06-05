4a23970dc8ddc231c4d236620999dae164efa3be
Satyansh-Mishra <satyansh40606@gmail.com>
2025-06-05 23:31:05 +0530

    new commit

--- javascript.js
+++ javascript.js
@@ -1,2 +1,8 @@
-let array=["aray","new","two"]
-alert(array)
+// your JavaScript file
+const container = document.querySelector("#container");
+
+const content = document.createElement("div");
+content.classList.add("content");
+content.textContent = "This is the glorious text-content!";
+content
+container.appendChild(content);
